5b5ed63d91efdcef796f0ad90f1614293a683b2a
thanh.maiAI <thanh.mai@southtelecom.vn>
2024-11-16 20:56:19 +0700

    data validation

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationPipeline
 
-STAGE_NAME = "TEST"
+STAGE_NAME = "Data Investigation"
 
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>>>>>>>>{STAGE_NAME} started<<<<<<<<<<<")
         obj = DataIngestionPipeline()
         obj.init_data_ingestion()
+        logger.info(f">>>>>>>>>>>>{STAGE_NAME} completed<<<<<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise
+
+
+STAGE_NAME = "Data Validation"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>>>>>>{STAGE_NAME} started<<<<<<<<<<<")
+        obj = DataValidationPipeline()
+        obj.init_data_validation()
         logger.info(f">>>>>>>>>>>>{STAGE_NAME} completed<<<<<<<<<<<")
     except Exception as e:
         logger.exception(e)
